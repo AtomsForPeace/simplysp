c53fe3ec08873fbfc518197559b90cbeabfe6da4
Adam Bannister <adam.p.bannister@gmail.com>
2018-08-11 21:50:37 +0200

    add basic prompt that outputs the input

---
+++ prompt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main(int argc, char** argv) {
+
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+
+	while (1) {
+
+		/* Now in either case readline will be correctly defined */
+		char* input = readline("lispy> ");
+		add_history(input);
+
+		printf("No you're a %s\n", input);
+		free(input);
+
+	}
+
+	return 0;
+}
