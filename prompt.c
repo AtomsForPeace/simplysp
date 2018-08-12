1a8d95396834d1db86f9663c103ac5fec575f131
Adam Bannister <adam.p.bannister@gmail.com>
2018-08-12 17:49:57 +0200

    add evaluation

--- prompt.c
+++ prompt.c
@@ -2,12 +2,45 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+	/* If tagged as number return it directly. */
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	/* The operator is always second child. */
+	char* op = t->children[1]->contents;
+
+	/* We store the third child in `x` */
+	long x = eval(t->children[2]);
+
+	/* Iterate the remaining children and combining. */
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
+
 int main(int argc, char** argv) {
 	/* Create Some Parsers */
 	mpc_parser_t* Number   = mpc_new("number");
 	mpc_parser_t* Operator = mpc_new("operator");
 	mpc_parser_t* Expr     = mpc_new("expr");
-	mpc_parser_t* Lispy    = mpc_new("lispy");
+	mpc_parser_t* Simplysp = mpc_new("simplysp");
 
 	/* Define them with the following Language */
 	mpca_lang(MPCA_LANG_DEFAULT,
@@ -15,23 +48,24 @@ int main(int argc, char** argv) {
 			number   : /-?[0-9]+/ ;                             \
 			operator : '+' | '-' | '*' | '/' ;                  \
 			expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-			lispy    : /^/ <operator> <expr>+ /$/ ;             \
+			simplysp : /^/ <operator> <expr>+ /$/ ;             \
 		",
-		Number, Operator, Expr, Lispy);
+		Number, Operator, Expr, Simplysp);
 
-	puts("Lispy Version 0.0.0.0.1");
+	puts("simplysp version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 
 	while (1) {
 
 		/* Now in either case readline will be correctly defined */
-		char* input = readline("lispy> ");
+		char* input = readline("simplysp> ");
 		add_history(input);
 
 		mpc_result_t r;
-		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+		if (mpc_parse("<stdin>", input, Simplysp, &r)) {
 			/* On success print and delete the AST */
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			/* Otherwise print and delete the Error */
@@ -43,7 +77,7 @@ int main(int argc, char** argv) {
 
 	}
 
-	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+	mpc_cleanup(4, Number, Operator, Expr, Simplysp);
 
 	return 0;
 }
